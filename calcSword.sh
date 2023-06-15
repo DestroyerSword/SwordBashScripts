ceb566e2b6329afad0d174525401840def03343a
DestroyerSword <TheDestroyerSword@outlook.com>
2023-06-15 23:37:12 +0200

    resolved some bugs and comments

--- calcSword.sh
+++ calcSword.sh
@@ -7,7 +7,7 @@ print_header(){
 	clear
 	echo -e "\n\t|======== calcSword - Command ========|"
 	echo -e "\t| -To exit write 'q/Q/exit'"
-	echo -e "\t| -Basic operations mode: 'basicOp()'"
+	echo -e "\t| -Basic operations mode: 'basicOp'"
 	echo -e "\t|=====================================|\n"
 }
 print_header
@@ -22,7 +22,7 @@ while [ 1 ]; do
 	#Prints the prompt and read the line
 	read -p "cS-[Command]>> " input_command
 
-	#Conditioin to close the script -- Pressing Q
+	#Conditioin to close the script -- Pressing q/Q/exit 
 	if [[ $input_command = "q" ]] || [[ $input_command = "Q" ]] || [[ $input_command = "exit" ]]; then
 		#Exits the script after clearing the console
 		clear
@@ -30,7 +30,7 @@ while [ 1 ]; do
 	fi
 
 	#Basic operations mode
-	if [[ $input_command = "basicOp()" ]]; then
+	if [[ $input_command = "basicOp" ]]; then
 		$basicOp_path
 		print_header
 	fi
--- calcSword_functions/basic_op.sh
+++ calcSword_functions/basic_op.sh
@@ -6,13 +6,27 @@
 #Header of the basic operations script
 clear
 echo -e "\n\t|======== calcSword - Basic Operations ========|"
-echo -e "\t| -To exit write 'q/Q/exit' "
-echo -e "\t| -Basic operations: '+' / '-' / '*' / '/' / '%' / '**'"
-echo -e "\t|==============================================|\n"
 
-#Input variable
+### Help Menu ###
+help_menu(){
+	echo -e "\n\t|======== calcSword - Basic Operations ========|"
+	echo -e "\t| -To exit write 'q/Q/exit' "
+	echo -e "\t| -Basic arithmetic operators: \n\t|\tAddition: '+' \n\t|\tSubstraction: '-' \n\t|\tMultiiplication: '*' \n\t|\tDivision: '/' \n\t|\tModulus: '%'"
+	echo -e "\t|"
+	echo -e "\t| -Basic relational operators: \n\t|\tEqual: '==' \n\t|\tNot Equal: '!=' \n\t|\tLess than: '<' \n\t|\tLess than or equal: '<=' \n\t|\tGreater than: '>' \n\t|\tGreater than or equal: '>='"
+	echo -e "\t|"
+	echo -e "\t| -Basic logical operators: \n\t|\tAND: '&&' \n\t|\tOR: '||' \n\t|\tNOT: '!'"
+	echo -e "\t|"
+	echo -e "\t| -Bitwise operators: \n\t|\tAnd: '&' \n\t|\tOr: '|' \n\t|\tXor: '^' \n\t|\tCompliment: '~' \n\t|\tLeft shift: '<<' \n\t|\tRight shift: '>>'"
+	echo -e "\t|==============================================|\n"
+}
+
+#Input string variable
 input_op=""
 
+#################
+### Main Loop ###
+#################
 while [ 1 ]; do
 
 	read -p "cs-[BasicOp]>> " input_op
@@ -23,5 +37,10 @@ while [ 1 ]; do
 		exit
 	fi
 
-	echo $((input_op))
+	if [[ $input_op = "help" ]]; then
+		help_menu
+	fi
+
+	echo $(($input_op))
+
 done
